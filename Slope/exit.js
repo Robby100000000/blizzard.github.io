ac83c109b2fb3edd27d3d060dc760e34e308988a
Robby100000000 <83455866+Robby100000000@users.noreply.github.com>
2025-03-26 08:40:56 -0500

    Update exit.js

--- Slope/exit.js
+++ Slope/exit.js
@@ -1,6 +1,6 @@
-// Enable navigation prompt
-window.onbeforeunload = function() {
-    return true;
-};
-// Remove navigation prompt
-window.onbeforeunload = null;
+// Show "Leave Site?" Dialog Box
+
+window.addEventListener("beforeunload", (event) => {
+  // set a truthy value to property returnValue
+  event.returnValue = true;
+});
